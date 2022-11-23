ac978a8255cd90d0704330175e2ab5d32d73158b
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-24 00:48:27 +0300

    morrrre

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,8 +1,31 @@
-
+import {useState} from 'react'
 const UploadForm = () =>{
+    const [file, setFile] = useState(null)
+    const [error, setError] = useState(null)
+
+    const types = ['image/png','image/jpeg']
+
+
+    const changeHandler = (e) =>{
+        let selected = e.target.files[0];
+       
+        if (selected && types.icnlude(selected.type)){
+            setFile(selected)
+            setError('')
+        }
+        else{
+            setFile(null);
+            setError('please select an image file(png or jpeg)')
+
+        }
+    }
     return(
         <form>
-            <input type = 'file' />
+            <input type = 'file' onChange={changeHandler} />
+            <div className='output' >
+                {error && <div className='error'>{error} </div>}
+                {file && <div>{file.name}</div>}
+            </div>
         </form>
     )
 }
